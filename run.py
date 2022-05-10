5b40c348dd2f47a8e5c1e68343b77fa4423c6c9d
Kaysheridan <kaysheridan1@hotmail.com>
2022-05-10 20:59:28 +0000

    removed commented out code

--- run.py
+++ run.py
@@ -17,21 +17,6 @@ def game_loop():
     The First page to appear on the terminal, used to present the player
     with print statements
     """
- #   while True:
- #       player_guess = int(input("Take a guess.."))
- #      guess_amount = +1
- #       if correct_answer != player_guess:
- #           print("Woopsies, try again...")
- #           if correct_answer < player_guess:
- #               txt = "You are getting warmer the right number is between 1 and {}"
- #               print(txt.format(player_guess))
- #           else:
- #               txt_two = "You're getting warmer the right number is between {} and 10"
- #               print(txt_two.format(player_guess))
- #       else:
- #           txt_three = "YOU GOT IT! Guesses to beat {}"
- #           print(txt_three.format(guess_amount))
- #           break
     for guesses in range(10):
         print('Take a guess...')
         while True:
